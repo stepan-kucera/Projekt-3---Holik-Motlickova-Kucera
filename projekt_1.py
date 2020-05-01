07be30238d8ece7ab726a9ac6cca1a615a875087
klaramotlic <62472991+klaramotlic@users.noreply.github.com>
2020-05-01 19:54:28 +0200

    Update projekt_1.py

--- projekt_1.py
+++ projekt_1.py
@@ -33,24 +33,36 @@ def print_errors(error_list):
     for word in error_list:
         print(word)
 
+def first_order_variants(word):
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [a + b[1:] for a, b in splits if b]
+    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b) > 1]
+    replaces = [a + c + b[1:] for a, b in splits for c in abeceda if b]
+    inserts = [a + c + b for a, b in splits for c in abeceda]
+    return set(deletes + transposes + replaces + inserts)
 
-def edits1(word):
-   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-   deletes    = [a + b[1:] for a, b in splits if b]
-   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-   inserts    = [a + c + b     for a, b in splits for c in alphabet]
-   return set(deletes + transposes + replaces + inserts)
+
+def second_order_variants(word):
+    return set(e2 for e1 in first_order_variants(word) for e2 in first_order_variants(e1) if e2 in dictionary)
 
 
-def known(words): return set(w for w in words if w in word)
+def known(words):
+    return set(w for w in words if w in dictionary)
+
 
 def correct(word):
-    candidates = known([word]) or known(edits1(word)) or \
-        known_edits2(word) or [word]
-    print("Návrhy na opravu jsou:")
-    return max(candidates, key=misspelled_words.get)
-    print(candidates)
+    candidates = known(word) or known(*first_order_variants(word)) or second_order_variants(word) or [word]
+    return max(candidates, key=dictionary.get)
+
+
+def guesses(word):
+    result = list(known(*first_order_variants(word)))
+    result.sort()
+    return result
+
+
+def add(word, priority=4):
+    dictionary[word.lower().strip()] = priority
 
 
 def main():
